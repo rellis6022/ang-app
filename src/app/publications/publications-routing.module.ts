61e2066810b17b661f17fd33918d64655a80d06b
MacPro Josh <josh.coolman@singlemindconsulting>
2021-04-13 09:01:51 -0700

    add child routes in Publications

--- src/app/publications/publications-routing.module.ts
+++ src/app/publications/publications-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
+import { ResourcesComponent } from "./resources/resources.component";
+import { ArticleComponent } from "./article/article.component";
 
 const routes: Routes = [
-  {path:'publications', component:HomeComponent}
+  {
+    path:'publications', 
+    component:HomeComponent,
+    children:[
+      {path:'resources', component:ResourcesComponent},
+      {path:'article', component:ArticleComponent}
+    ]
+  }
 ];
 
 @NgModule({
