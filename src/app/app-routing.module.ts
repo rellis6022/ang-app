ef700ce7d889f6ea301c56423abbacbdd13562ce
MacPro Josh <josh.coolman@singlemindconsulting>
2021-04-14 09:25:00 -0700

    simplify.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from "./homepage/homepage.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
-import { MockDataSandboxComponent } from "./mock-data-sandbox/mock-data-sandbox.component";
 
 const routes: Routes = [
   {path:'', component:HomepageComponent},
-  {path:'sandbox', component:MockDataSandboxComponent},
   {path:'**', component:NotFoundComponent}
 ];
 
