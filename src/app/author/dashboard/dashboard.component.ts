388ed2e3e1b2442ca9dffbf44b2293cbad6559c4
MacPro Josh <josh.coolman@singlemindconsulting>
2021-04-16 18:29:21 -0700

    custom styling for ckeditor

--- src/app/author/dashboard/dashboard.component.ts
+++ src/app/author/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import * as ClassicEditor from '@uxui-guy/ckeditor-custom';
 
 
 @Component({
--- src/ckeditor.d.ts
+++ src/ckeditor.d.ts
@@ -1,5 +1,9 @@
+declare module '@uxui-guy/ckeditor-custom' {
+    const ClassicEditorBuild: any;
+    export = ClassicEditorBuild;
+}
+
 declare module '@ckeditor/ckeditor5-build-classic' {
     const ClassicEditorBuild: any;
-
     export = ClassicEditorBuild;
 }
