87040b313b4f96e6b92b78b1a0910c04907e909e
MacPro Josh <josh.coolman@singlemindconsulting>
2021-04-19 18:17:00 -0700

    authoring markup

--- src/app/author/dashboard/dashboard.component.ts
+++ src/app/author/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import * as ContentEditor from '@uxui-guy/ckeditor5-custom-inline';
 
 
+
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -9,7 +11,20 @@ import * as ContentEditor from '@uxui-guy/ckeditor5-custom-inline';
 })
 export class DashboardComponent implements OnInit {
 
-  public Editor = ContentEditor;
+  public Editor = ContentEditor
+  content = `
+        <h4>Authoring</h4>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel perspiciatis tempore qui distinctio obcaecati. Quidem saepe similique officia tempore eaque dicta laudantium quia ipsum beatae dolore rerum, non dignissimos, suscipit reiciendis voluptatum? Incidunt maiores eaque nobis praesentium placeat esse voluptatibus, sapiente consequatur, cupiditate reiciendis ea tenetur ullam quos autem possimus!
+        </p>`
+
+  onChange(ev:any){
+    console.log(ev.editor)
+  }
+
+  onSave() {
+    console.log('onSave')
+    
+  }
 
   constructor() { }
 
