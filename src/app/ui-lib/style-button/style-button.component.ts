909f344f2cadd3a9af40cca5715d112e58e07525
MacPro Josh <josh.coolman@singlemindconsulting>
2021-04-23 22:00:33 -0700

    button styles

--- src/app/ui-lib/style-button/style-button.component.ts
+++ src/app/ui-lib/style-button/style-button.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-style-button',
   templateUrl: './style-button.component.html',
-  styleUrls: ['./style-button.component.css']
 })
 export class StyleButtonComponent implements OnInit {
 
