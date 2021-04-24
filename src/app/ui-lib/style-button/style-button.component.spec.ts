0c5c67590bb245cdf7bd6062f4124fcb89dafe3d
MacPro Josh <josh.coolman@singlemindconsulting>
2021-04-23 18:30:15 -0700

    button styles

---
+++ src/app/ui-lib/style-button/style-button.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StyleButtonComponent } from './style-button.component';
+
+describe('StyleButtonComponent', () => {
+  let component: StyleButtonComponent;
+  let fixture: ComponentFixture<StyleButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ StyleButtonComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StyleButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/ui-lib/style-button/style-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-style-button',
+  templateUrl: './style-button.component.html',
+  styleUrls: ['./style-button.component.css']
+})
+export class StyleButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
